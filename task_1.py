78f21c8207c34032dfafedf593909d48fbc282c2
Geusj <regby19999@gmail.com>
2023-04-12 11:51:56 +0200

    HW11

---
+++ task_1.py
@@ -0,0 +1,37 @@
+phone_book = {}
+
+while True:
+    user_data = input("Enter data(use command :add,delete,stats,show,list):")
+    if user_data == "last":
+        break
+
+    data = user_data.split()
+    command = data[0]
+    name = data[1] if len(data) > 1 else None
+
+    if command == "add":
+        phone = data[2]
+        if name and name not in phone_book:
+            phone_book[name] = phone
+            try:
+                value = int(phone)
+            except ValueError:
+                print("The phone number can only contain numbers")
+    elif command == "delete":
+        try:
+            value = phone_book[name]
+        except KeyError:
+            print("This user is not in the phone book")
+        if name and name in phone_book:
+            del phone_book[name]
+    elif command == "stats":
+        stats = f"Number of entries: {len(phone_book.keys())}"
+        print(stats)
+    elif command == "list":
+        names = list(phone_book.keys())
+        print(names)
+    elif command == "show":
+        if name and name in phone_book:
+            print(f"Name: {name}, Phone: {phone_book[name]}")
+    elif command != "show" and command != "list" and command != "stats" and command != "delete" and command != "add":
+        print("This command not exist")
