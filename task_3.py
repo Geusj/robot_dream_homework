3c267811997b8ccce6d929e34e95fcacd5c726ad
Geusj <regby19999@gmail.com>
2023-05-01 19:07:59 +0200

    HW15

--- task_3.py
+++
@@ -1,5 +0,0 @@
-import re
-
-record = "+380996378547, 0996378547, 380996378547, 552656589"
-find_phones = re.findall(r"(?:\+38|38|0)\d{9}(?=[, ]|$)", record)
-print(f"This customer from Ukraine {find_phones}")
