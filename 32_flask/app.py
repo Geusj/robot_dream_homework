7c30ca1bab65ece44feceffb7b248ad51cab3218
Geusj <regby19999@gmail.com>
2023-07-10 09:58:52 +0200

    HW_33

--- 32_flask/app.py
+++ 32_flask/app.py
@@ -142,6 +142,7 @@ def validate_username(username):
     return False
 
 
+
 def validate_password(password):
     if len(password) >= 8 and any(char.isdigit() for char in password) and any(char.isupper() for char in password):
         return True
